98511116d422be71dffbc59b3c18537b4a3be2ec
draco <122390064+Darco786@users.noreply.github.com>
2023-02-10 03:43:27 +0530

    new

--- src/components/nftUserHome/nftUserHome.js
+++ src/components/nftUserHome/nftUserHome.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import NftUserProfile from './NftUserProfile'
 import './UserBanner.css'
+import Line1 from '../../assets/line3.png'
+import Line2 from '../../assets/line4.png'
+import Line3 from '../../assets/line1.png'
+import Line4 from '../../assets/line2.png'
+
 function NftUserHome() {
   return (
     <>
     <section className='user_head_banner'>
+
+      <div className='side-border'>
+        <img src={Line1} alt=""/>
+        <img src={Line2} alt=""/>
+
+      </div>
+
+      <div className='side-border2'>
+        <img src={Line3} alt=""/>
+        <img src={Line4} alt=""/>
+
+      </div>
       
     </section>
     <NftUserProfile/>
